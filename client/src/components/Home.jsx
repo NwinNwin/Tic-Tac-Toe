add1bf8b4b92918ee683ce5d961af2584da57611
NwinNwin <ndnguyen2003@gmail.com>
2023-01-03 12:23:59 -0800

    added logo to home and description text for games

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,39 +5,48 @@ import robot from "../images/robot.svg";
 
 export default function Home({ setMultiplayerPopUp, setOfflineGame, setBotGame }) {
   return (
-    <div className="home">
-      {/*Multiplayer Game Button */}
-      <div
-        onClick={() => {
-          setMultiplayerPopUp(true);
-        }}
-        className="home-btn"
-        title="Online Game"
-      >
-        <img src={online} alt="" className="home-btn-img" />
+    <>
+      <div className="title">
+        <h1 className="title-1">Tic-tac-toe +</h1>
       </div>
+      <div className="home">
+        {/*Multiplayer Game Button */}
 
-      {/* Bot Game Button */}
-      <div
-        onClick={() => {
-          setBotGame(true);
-        }}
-        className="home-btn"
-        title="Bot Game"
-      >
-        <img src={robot} alt="" className="home-btn-img" />
-      </div>
+        <div
+          onClick={() => {
+            setMultiplayerPopUp(true);
+          }}
+          className="home-btn"
+          title="Online Game"
+        >
+          <img src={online} alt="" className="home-btn-img" />
+          <h3>With Friends</h3>
+        </div>
 
-      {/*Offline Game Button */}
-      <div
-        onClick={() => {
-          setOfflineGame(true);
-        }}
-        className="home-btn"
-        title="Offline Game"
-      >
-        <img src={offline} alt="" className="home-btn-img" />
+        {/* Bot Game Button */}
+        <div
+          onClick={() => {
+            setBotGame(true);
+          }}
+          className="home-btn"
+          title="Bot Game"
+        >
+          <img src={robot} alt="" className="home-btn-img" />
+          <h3>AI</h3>
+        </div>
+
+        {/*Offline Game Button */}
+        <div
+          onClick={() => {
+            setOfflineGame(true);
+          }}
+          className="home-btn"
+          title="Offline Game"
+        >
+          <img src={offline} alt="" className="home-btn-img" />
+          <h3>Offline</h3>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
