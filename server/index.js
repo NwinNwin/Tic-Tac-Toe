0a17aff380a4cfb693b52be6c663c42dd85e9181
NwinNwin <ndnguyen2003@gmail.com>
2023-01-03 21:16:25 -0800

    added front-end link

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://tic-tac-toe-plus-client.vercel.app/",
     method: ["GET", "POST"],
   },
 });
