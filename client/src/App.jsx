d00c24ca98a9310beaa0b5fb59e7adb01fbe1c76
NwinNwin <ndnguyen2003@gmail.com>
2023-01-07 11:12:58 -0800

    last commit, fixed env

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import BotGame from "./components/BotGame";
 
-const socket = io.connect("https://nwin-tic-tac-toe.onrender.com");
+const socket = io.connect(process.env.REACT_APP_SOCKETAPI);
 
 function App() {
   const [username, setUsername] = useState("");
